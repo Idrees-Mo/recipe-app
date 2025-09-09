25374daedc777604e4d4b972ced2dc01584e49d0
Muhammad Idrees <idris@y7mail.com>
2025-09-09 18:01:28 +1000

    wip: unit test in progres

--- src/app/pages/recipe-detail/recipe-detail.spec.ts
+++ src/app/pages/recipe-detail/recipe-detail.spec.ts
@@ -1,16 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipeDetail } from './recipe-detail';
+import { provideHttpClient } from '@angular/common/http';
 
-describe('RecipeDetail', () => {
+xdescribe('RecipeDetail', () => {
   let component: RecipeDetail;
   let fixture: ComponentFixture<RecipeDetail>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RecipeDetail]
-    })
-    .compileComponents();
+      imports: [RecipeDetail],
+      providers: [provideHttpClient()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RecipeDetail);
     component = fixture.componentInstance;
