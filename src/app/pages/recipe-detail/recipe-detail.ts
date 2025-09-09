0267c5d8e00abdb089edbbb624d00bfc45c54a5d
Muhammad Idrees <idris@y7mail.com>
2025-09-09 17:06:35 +1000

    feat: recipe-detail

--- src/app/pages/recipe-detail/recipe-detail.ts
+++ src/app/pages/recipe-detail/recipe-detail.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Recipe } from '../../core/models/recipe';
+import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from '../../core/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   imports: [],
   templateUrl: './recipe-detail.html',
-  styleUrl: './recipe-detail.scss'
+  styleUrl: './recipe-detail.scss',
 })
-export class RecipeDetail {
+export class RecipeDetail implements OnInit {
+  meal: Recipe | null = null;
+  loading = true;
 
+  // private recipeSvc = inject(RecipeService);
+  private route = inject(ActivatedRoute);
+
+  constructor(private recipeSvc: RecipeService) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.recipeSvc.getMealById(id).subscribe((m) => {
+      this.meal = m;
+      this.loading = false;
+    });
+  }
 }
