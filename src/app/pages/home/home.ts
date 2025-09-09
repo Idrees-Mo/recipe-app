55519c6e8bba69fa75ac1438426e72c474da8809
Muhammad Idrees <idris@y7mail.com>
2025-09-09 15:23:30 +1000

    wip: use service to get recipe from api

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Recipe } from '../../core/models/recipe';
 import { mockRecipes } from '../../core/mocks/recipe.mock';
 import { RecipeCard } from '../../shared/components/recipe-card/recipe-card';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { RecipeService } from '../../core/services/recipe.service';
 
 @Component({
   selector: 'app-home',
-  imports: [RecipeCard, NgFor],
+  imports: [RecipeCard, NgFor, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
@@ -15,8 +17,16 @@ export class Home implements OnInit {
   recipes: Recipe[] = [];
   loading = false;
 
-  constructor() {}
+  constructor(private recipeSvc: RecipeService) {}
   ngOnInit() {
     this.recipes = mockRecipes;
+  }
+
+  onSearch() {
+    this.loading = true;
+    this.recipeSvc.searchMeals(this.query).subscribe((list) => {
+      this.recipes = list;
+      this.loading = false;
+    });
   }
 }
