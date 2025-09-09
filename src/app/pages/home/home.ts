a8afbf21831cbbb48fb33aa8e5a7ac9e08015c2e
Muhammad Idrees <idris@y7mail.com>
2025-09-09 14:39:46 +1000

    add basic card with mock-data

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../core/models/recipe';
+import { mockRecipes } from '../../core/mocks/recipe.mock';
+import { RecipeCard } from '../../shared/components/recipe-card/recipe-card';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RecipeCard, NgFor],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+  recipes: Recipe[] = [];
+  loading = false;
 
+  constructor() {}
+  ngOnInit() {
+    this.recipes = mockRecipes;
+  }
 }
--- src/app/shared/components/recipe-card/recipe-card.ts
+++ src/app/shared/components/recipe-card/recipe-card.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../../core/models/recipe';
+import { RouterLink } from '@angular/router';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-card',
-  imports: [],
+  imports: [RouterLink, SlicePipe],
   templateUrl: './recipe-card.html',
-  styleUrl: './recipe-card.scss'
+  styleUrl: './recipe-card.scss',
 })
 export class RecipeCard {
-
+  @Input({ required: true }) recipe!: Recipe;
+  constructor() {
+    console.warn(this.recipe);
+  }
 }
