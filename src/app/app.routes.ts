c924239541b47eb13c3716dcfbf292889d2530c2
Muhammad Idrees <idris@y7mail.com>
2025-09-09 14:02:51 +1000

    add folder structure and basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+// import { Results } from './pages/results/results';
+import { RecipeDetail } from './pages/recipe-detail/recipe-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  //   { path: 'results', component: Results },
+  { path: 'recipe/:id', component: RecipeDetail },
+  { path: '**', redirectTo: '' },
+];
